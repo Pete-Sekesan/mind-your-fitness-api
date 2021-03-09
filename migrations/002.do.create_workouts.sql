6750af5c4db213c4f104abe3484acf8ad450aab4
Peter Sekesan <psekesan@gmail.com>
2021-03-09 17:08:42 -0500

    tied relationships with users and workouts tables

--- migrations/002.do.create_workouts.sql
+++ migrations/002.do.create_workouts.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS workouts;
 
 CREATE TABLE workouts (
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES 
+  users_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
   workout_name TEXT NOT NULL,
   date_created TIMESTAMPTZ DEFAULT now() NOT NULL,
   duration numeric
