f97f98a8e48c1b14fe3503775664558032a7d6f7
Peter Sekesan <psekesan@gmail.com>
2021-03-14 20:24:30 -0400

    added ENUM to workout table

--- migrations/002.do.create_workouts.sql
+++ migrations/002.do.create_workouts.sql
@@ -1,5 +1,20 @@
 DROP TABLE IF EXISTS workouts;
 
+DROP TYPE IF EXISTS workout_name;
+CREATE TYPE workout_name AS ENUM (
+    'Running',
+    'Walking',
+    'Hiking',
+    'Swimming',
+    'Cycling',
+    'Spin',
+    'Weightlifting',
+    'Yoga',
+    'HIIT',
+    'Elliptical',
+    'Other'
+);
+
 CREATE TABLE workouts (
   id SERIAL PRIMARY KEY,
   users_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
