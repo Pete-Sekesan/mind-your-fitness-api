ba0787c8aae0986823c9c0e143aa61b8ec258f15
Peter Sekesan <psekesan@gmail.com>
2021-03-16 14:33:39 -0400

    added users/workouts/auth routes and services

---
+++ src/middleware/dist/jwt.auth.dev.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var AuthService = require("../auth/auth-service");
+
+function requireAuth(req, res, next) {
+  var authToken = req.get("Authorization") || "";
+  var bearerToken;
+
+  if (!authToken.toLowerCase().startsWith("bearer ")) {
+    return res.status(401).json({
+      error: "Missing bearer token"
+    });
+  } else {
+    bearerToken = authToken.slice(7, authToken.length);
+  }
+
+  try {
+    var payload = AuthService.verifyJwt(bearerToken);
+    AuthService.getUserWithUsername(req.app.get("db"), payload.sub).then(function (user) {
+      if (!user) {
+        return res.status(401).json({
+          error: "Unauthorized request"
+        });
+      }
+
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    return res.status(401).json({
+      error: "Unauthorized request"
+    });
+  }
+}
+
+module.exports = {
+  requireAuth: requireAuth
+};
---
+++ src/users/dist/users-router.dev.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var express = require("express");
+
+var xss = require("xss");
+
+var usersRouter = express.Router();
+
+var UsersService = require("./users-service");
+
+var _require = require("../middleware/jwt-auth"),
+    requireAuth = _require.requireAuth;
+
+var serializeUser = function serializeUser(user) {
+  return {
+    id: user.id,
+    username: xss(user.username)
+  };
+};
+
+usersRouter.route("/").all(function (req, res, next) {
+  knexInstance = req.app.get("db");
+  next();
+}).get(requireAuth, function (req, res) {
+  res.json(serializeUser(req.user));
+}).post(function (req, res) {
+  var _req$body = req.body,
+      username = _req$body.username,
+      password = _req$body.password;
+  var REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
+
+  for (var _i = 0, _arr = ["username", "password"]; _i < _arr.length; _i++) {
+    var field = _arr[_i];
+
+    if (!req.body[field]) {
+      return res.status(400).json({
+        error: "Missing ".concat(field)
+      });
+    }
+  }
+
+  if (password.length < 8) {
+    return res.status(400).json({
+      error: "Password must be 8 or more characters"
+    });
+  }
+
+  if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+    return res.status(400).json({
+      error: "Password must contain one upper case character, one lower case character, one number, and one special character"
+    });
+  }
+
+  UsersService.hasUserWithUsername(knexInstance, username).then(function (hasUser) {
+    if (hasUser) {
+      return res.status(400).json({
+        error: "Username already exists"
+      });
+    }
+
+    return UsersService.hashPassword(password).then(function (hashedPassword) {
+      var newUser = {
+        username: username,
+        password: hashedPassword
+      };
+      return UsersService.insertUser(knexInstance, newUser).then(function (user) {
+        res.status(201).json(serializeUser(user));
+      });
+    });
+  });
+});
+module.exports = usersRouter;
