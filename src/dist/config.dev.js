a8ff8c8234033be821e5d7fe995fad4c3cfa1ff2
Peter Sekesan <psekesan@gmail.com>
2021-04-07 11:06:59 -0400

    heroku

---
+++ src/dist/config.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+  PORT: process.env.PORT || 8001,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DB_URL: process.env.DB_URL || "postgresql://petersekesan@localhost/mind-your-fitness"
+};
