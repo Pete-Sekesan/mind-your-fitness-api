1f0a4ce289ecd3b8c6df8631513df82c6f67f756
Peter Sekesan <psekesan@gmail.com>
2021-04-07 21:44:07 -0400

    reverted back to no header

--- src/dist/app.dev.js
+++ src/dist/app.dev.js
@@ -25,12 +25,6 @@ app.use(express.json());
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
-app.use(function (req, res, next) {
-  // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Origin", "https://mind-your-fitness.vercel.app/.TLD");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/workouts", workoutsRouter);
