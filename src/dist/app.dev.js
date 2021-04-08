7e14159f69c5efc3bdaaae85967bab84bf6b07c5
Peter Sekesan <psekesan@gmail.com>
2021-04-08 09:23:36 -0400

    regex for cors origin

--- src/dist/app.dev.js
+++ src/dist/app.dev.js
@@ -24,7 +24,7 @@ var morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(express.json());
 app.use(morgan(morganOption));
 app.use(cors({
-  origin: "*"
+  origin: /\.herokuapp\.com$/
 }));
 app.use(helmet());
 app.use("/api/auth", authRouter);
