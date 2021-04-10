7bcb620a824d797d2f019ec2f04750386a738fcd
Peter Sekesan <psekesan@gmail.com>
2021-04-10 08:54:02 -0400

    client origin cors

--- src/dist/app.dev.js
+++ src/dist/app.dev.js
@@ -24,7 +24,9 @@ var app = express();
 var morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(express.json());
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}));
 app.use(helmet());
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
